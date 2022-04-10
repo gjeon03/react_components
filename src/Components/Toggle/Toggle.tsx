32d404335101b2383b72909e795962d73b2cb00b
gjeon03 <gjeon0321@gmail.com>
2022-04-11 01:26:30 +0900

    route components

---
+++ src/Components/Toggle/Toggle.tsx
@@ -0,0 +1,8 @@
+export default function Toggle() {
+	console.log("hello");
+	return (
+		<div>
+			Hello
+		</div>
+	);
+}
---
+++ src/Routes/Components.tsx
@@ -0,0 +1,7 @@
+import Toggle from "../Components/Toggle/Toggle";
+
+export default function Components() {
+	return (
+		<Toggle />
+	);
+}
