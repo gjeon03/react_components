eedbd3ce51cf41881f0e91cfde7ce31199d475c8
gjeon03 <gjeon0321@gmail.com>
2022-04-26 17:30:33 +0900

    create Tab under bar

--- src/Components/Tab/Tab.tsx
+++ src/Components/Tab/Tab.tsx
@@ -3,40 +3,59 @@ import styled from "styled-components";
 import { Area, Title } from "../../Style/Styles";
 
 const TabForm = styled.form`
-	width: 500px;
+	width: 600px;
 	height: 30px;
 	display: grid;
 	grid-template-columns: repeat(3, 1fr);
+	border-bottom: 2px solid #EBEBEB;
+	position: relative;
 `;
 
-const Item = styled.label`
+const Item = styled.label<{ $checked: boolean }>`
 	width: 100%;
 	height: 100%;
-	border: 1px solid pink;
 	text-align: center;
 	font-size: 16px;
 	font-weight: 600;
 	position: relative;
+	cursor: pointer;
+	color: ${props => props.$checked ? "black" : "#aaaaaa"};
 `;
 
 const Radio = styled.input`
-	cursor: pointer;
 	position: absolute;
 	opacity: 0;
 `;
 
+const TabLabel = styled.label<{ $checked: any[] }>`
+	width: 200px;
+	height: 2px;
+	border-radius: 100px;
+	position: absolute;
+	bottom: -2px;
+	background-color: #10AEAF;
+	transition: all 0.2s ease-in-out;
+	${(props) => {
+		//const size = props.$checked.filter((v, i) => v.checked});
+		return props.$checked[1].checked &&
+			`
+		transform: translate(200px, 0);
+		transition: all 0.2s ease-in-out;
+		`
+	}}
+`;
+
 export default function Tab() {
 	const [onTab, setOnTab] = useState([
-		{ name: "감자", checked: true },
-		{ name: "고구마", checked: false },
-		{ name: "카레라이스", checked: false }
+		{ index: 0, name: "감자", checked: true },
+		{ index: 1, name: "고구마", checked: false },
+		{ index: 2, name: "카레라이스", checked: false }
 	]);
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		console.log(event.currentTarget.value);
 		setOnTab(onTab.map((v, i) => {
 			if (i + "" === event.currentTarget.value)
-				return { name: v.name, checked: true };
-			return { name: v.name, checked: false };
+				return { ...v, checked: true };
+			return { ...v, checked: false };
 		}));
 	};
 	return (
@@ -45,7 +64,7 @@ export default function Tab() {
 			<TabForm method="getea">
 				{onTab.map((v, i) => {
 					return (
-						<Item key={i + 1}>
+						<Item key={i + 1} $checked={v.checked}>
 							<Radio onChange={handleChange}
 								type="radio"
 								checked={v.checked}
@@ -55,6 +74,7 @@ export default function Tab() {
 						</Item>
 					)
 				})}
+				<TabLabel $checked={onTab} />
 			</TabForm>
 		</Area>
 	);
